8346383fda46c0983023d5f8997656dab4a0fdc6
PELIME <il>
2020-05-24 16:51:07 +0800

    完善短信群发

--- ebanckcommon/src/main/java/com/pelime/newbanck/shiro/ShiroConfig.java
+++ ebanckcommon/src/main/java/com/pelime/newbanck/shiro/ShiroConfig.java
@@ -29,12 +29,12 @@ public class ShiroConfig {
         filterChainDefinitionMap.put("/login", "anon");
         filterChainDefinitionMap.put("/regiest", "anon");
         filterChainDefinitionMap.put("/static/**", "anon");
-        filterChainDefinitionMap.put("/", "anon");
-        filterChainDefinitionMap.put("/front/**", "anon");
-        filterChainDefinitionMap.put("/api/**", "anon");
+        //filterChainDefinitionMap.put("/", "anon");
+        //filterChainDefinitionMap.put("/front/**", "anon");
+        filterChainDefinitionMap.put("/api/login", "anon");
 
-        filterChainDefinitionMap.put("/admin/**", "authc");
-        filterChainDefinitionMap.put("/user/**", "authc");
+        //filterChainDefinitionMap.put("/admin/**", "authc");
+        //filterChainDefinitionMap.put("/user/**", "authc");
         //主要这行代码必须放在所有权限设置的最后，不然会导致所有 url 都被拦截 剩余的都需要认证
         filterChainDefinitionMap.put("/**", "authc");
         shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
